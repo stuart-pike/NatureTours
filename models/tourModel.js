3f030193a9743a62b6149d1fe593036bdabf09f6
Stuart Pike <110692951+stuart-pike@users.noreply.github.com>
2025-10-30 14:24:09 -0700

    Implemented document, query and aggregation middleware to model

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,76 +1,130 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour must have a name'],
-    unique: true,
-    trim: true,
-    maxlength: [40, 'A tour name must have less or equal than 40 characters'],
-    minlength: [5, 'A tour name must have more or equal than 5 characters'],
-  },
-  duration: {
-    type: Number,
-    required: [true, 'A tour must have a duration'],
-  },
-  maxGroupSize: {
-    type: Number,
-    required: [true, 'A tour must have a group size'],
-  },
-  difficulty: {
-    type: String,
-    required: [true, 'A tour must have a difficulty'],
-    enum: {
-      values: ['easy', 'medium', 'difficult'],
-      message: 'Difficulty must be either: easy, medium, or difficult',
+const tourSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'A tour must have a name'],
+      unique: true,
+      trim: true,
+      maxlength: [40, 'A tour name must have less or equal than 40 characters'],
+      minlength: [5, 'A tour name must have more or equal than 5 characters'],
     },
-  },
-  ratingsAverage: {
-    type: Number,
-    default: 4.5,
-    min: [1, 'Rating must be above 1.0'],
-    max: [5, 'Rating must be below 5.0'],
-    set: (val) => Math.round(val * 10) / 10, // e.g. 4.666 => 4.7
-  },
-  ratingsQuantity: {
-    type: Number,
-    default: 0,
-  },
-  price: {
-    type: Number,
-    required: [true, 'A tour must have a price'],
-  },
-  priceDiscount: {
-    type: Number,
-    validate: {
-      validator: function (val) {
-        // Custom validator to check that discount price is less than regular price
-        return val < this.price;
+    slug: String,
+    duration: {
+      type: Number,
+      required: [true, 'A tour must have a duration'],
+    },
+    maxGroupSize: {
+      type: Number,
+      required: [true, 'A tour must have a group size'],
+    },
+    difficulty: {
+      type: String,
+      required: [true, 'A tour must have a difficulty'],
+      enum: {
+        values: ['easy', 'medium', 'difficult'],
+        message: 'Difficulty must be either: easy, medium, or difficult',
       },
-      message: 'Discount price ({VALUE}) must be less than regular price',
+    },
+    ratingsAverage: {
+      type: Number,
+      default: 4.5,
+      min: [1, 'Rating must be above 1.0'],
+      max: [5, 'Rating must be below 5.0'],
+      set: (val) => Math.round(val * 10) / 10, // e.g. 4.666 => 4.7
+    },
+    ratingsQuantity: {
+      type: Number,
+      default: 0,
+    },
+    price: {
+      type: Number,
+      required: [true, 'A tour must have a price'],
+    },
+    priceDiscount: {
+      type: Number,
+      validate: {
+        validator: function (val) {
+          // Custom validator to check that discount price is less than regular price
+          return val < this.price;
+        },
+        message: 'Discount price ({VALUE}) must be less than regular price',
+      },
+    },
+    summary: {
+      type: String,
+      trim: true,
+      required: [true, 'A tour must have a summary'],
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    imageCover: {
+      type: String,
+      required: [true, 'A tour must have a cover image'],
+    },
+    images: [String],
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      select: false, // hides this field from query results by default
+    },
+    startDates: [Date],
+    secretTour: {
+      type: Boolean,
+      default: false,
     },
   },
-  summary: {
-    type: String,
-    trim: true,
-    required: [true, 'A tour must have a summary'],
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
   },
-  description: {
-    type: String,
-    trim: true,
-  },
-  imageCover: {
-    type: String,
-    required: [true, 'A tour must have a cover image'],
-  },
-  images: [String],
-  startDates: [Date], // You could convert these to Date objects later if you clean your data
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-    select: false, // hides this field from query results by default
-  },
+);
+
+tourSchema.virtual('durationWeeks').get(function () {
+  return this.duration / 7;
+});
+
+// Document Middleware: runs before .save() and .create() commands only
+tourSchema.pre('save', function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+// tourSchema.pre('save', function (next) {
+//   console.log('💾 Will save document...');
+//   next();
+// });
+
+// tourSchema.post('save', function (doc, next) {
+//   console.log('💾 Document saved:', doc);
+//   next();
+// });
+
+// Query Middleware: runs before .find() and .findOne() commands only
+// Query middleware to exclude secret tours from any find query
+// tourSchema.pre('find', function (next) {
+tourSchema.pre(/^find/, function (next) {
+  this.find({ secretTour: { $ne: true } });
+  this.start = Date.now();
+  next();
+});
+
+tourSchema.post(/^find/, function (docs, next) {
+  console.log('💾 Documents found:', docs);
+  console.log(`⏰ Query took ${Date.now() - this.start} milliseconds!`);
+  next();
+});
+
+// Aggregation Middleware: runs before .aggregate() commands only
+tourSchema.pre('aggregate', function (next) {
+  // Exclude secret tours from aggregation results
+  this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+  next();
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
